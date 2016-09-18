4dd2268c4996c6465de013084c0a1461f07d3372
mq <mq@162.com>
2016-09-18 11:49:14 +0800

    初始化项目

---
+++ firstProgram/firstProgram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  firstProgram
+//
+//  Created by 甘明强 on 16/9/18.
+//  Copyright © 2016年 xinjingxiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
